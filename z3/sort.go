87f7062298f6714510a4ffb104e76aafd1092b13
Austin Clements <austin@google.com>
2017-05-13 16:40:52 -0400

    z3: move Kind to kind.go

--- z3/sort.go
+++ z3/sort.go
@@ -6,7 +6,6 @@ package z3
 
 import (
 	"runtime"
-	"strconv"
 )
 
 /*
@@ -32,9 +31,6 @@ type sortImpl struct {
 	c    C.Z3_sort
 	kind Kind
 }
-
-// kindWrappers is a map of Expr constructors for each sort kind.
-var kindWrappers = make(map[Kind]func(x expr) Expr)
 
 func wrapSort(ctx *Context, c C.Z3_sort, kind Kind) Sort {
 	if kind == KindUnknown {
@@ -62,55 +58,6 @@ func (sort Sort) String() string {
 	})
 	runtime.KeepAlive(sort)
 	return res
-}
-
-// Kind is a general category of sorts, such as int or array.
-type Kind int
-
-const (
-	KindUninterpreted = Kind(C.Z3_UNINTERPRETED_SORT)
-	KindBool          = Kind(C.Z3_BOOL_SORT)
-	KindInt           = Kind(C.Z3_INT_SORT)
-	KindReal          = Kind(C.Z3_REAL_SORT)
-	KindBV            = Kind(C.Z3_BV_SORT)
-	KindArray         = Kind(C.Z3_ARRAY_SORT)
-	KindDatatype      = Kind(C.Z3_DATATYPE_SORT)
-	KindRelation      = Kind(C.Z3_RELATION_SORT)
-	KindFiniteDomain  = Kind(C.Z3_FINITE_DOMAIN_SORT)
-	KindFloatingPoint = Kind(C.Z3_FLOATING_POINT_SORT)
-	KindRoundingMode  = Kind(C.Z3_ROUNDING_MODE_SORT)
-	KindUnknown       = Kind(C.Z3_UNKNOWN_SORT)
-)
-
-// String returns k as a string like "KindBool".
-func (k Kind) String() string {
-	switch k {
-	case KindUninterpreted:
-		return "KindUninterpreted"
-	case KindBool:
-		return "KindBool"
-	case KindInt:
-		return "KindInt"
-	case KindReal:
-		return "KindReal"
-	case KindBV:
-		return "KindBV"
-	case KindArray:
-		return "KindArray"
-	case KindDatatype:
-		return "KindDatatype"
-	case KindRelation:
-		return "KindRelation"
-	case KindFiniteDomain:
-		return "KindFiniteDomain"
-	case KindFloatingPoint:
-		return "KindFloatingPoint"
-	case KindRoundingMode:
-		return "KindRoundingMode"
-	case KindUnknown:
-		return "KindUnknown"
-	}
-	return "Kind(" + strconv.Itoa(int(k)) + ")"
 }
 
 // Kind returns s's kind.
