b40c8028126d002ec680d87d240644a385d298a5
Koki Kato <koki.kato1994@gmail.com>
2018-10-25 22:20:51 +0900

    z3: use stdint types
    __int64 and __uint64 were removed from z3_api.h and they use
    int64_t and uint64_t instead.

--- z3/finite.go
+++ z3/finite.go
@@ -7,6 +7,7 @@ package z3
 /*
 #cgo LDFLAGS: -lz3
 #include <z3.h>
+#include <stdint.h>
 */
 import "C"
 
@@ -36,7 +37,7 @@ func (ctx *Context) FiniteDomainSort(name string, n uint64) Sort {
 	sym := ctx.symbol(name)
 	var sort Sort
 	ctx.do(func() {
-		sort = wrapSort(ctx, C.Z3_mk_finite_domain_sort(ctx.c, sym, C.__uint64(n)), KindFiniteDomain)
+		sort = wrapSort(ctx, C.Z3_mk_finite_domain_sort(ctx.c, sym, C.uint64_t(n)), KindFiniteDomain)
 	})
 	return sort
 }
